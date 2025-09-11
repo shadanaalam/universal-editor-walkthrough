32805fc9792b3690ab39a201966b86c596d248be
shadanaalam <shadan.aalam@tothenew.com>
2025-09-11 12:00:27 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -37,47 +37,6 @@ export default async function decorate(block) {
         }
     });
 
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-import { loadJQuery } from '../../scripts/jquery-loader.js';
-
-export default async function decorate(block) {
-  const $ = await loadJQuery();
-
-  // Create UL list from block content
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-
-  block.textContent = '';
-  block.append(ul);
-
-  // Example AJAX request
-  $.ajax({
-    url: "/content/ttnretail/us/en/sign-in.infinity.json",
-    type: "GET",
-    success: function (data) {
-      console.log("Data: ", data);
-    },
-    error: function () {
-      console.warn("Could not fetch page properties");
-    }
-  });
-
 
   try {
     const res = await fetch("https://countriesnow.space/api/v0.1/countries/flag/images");
