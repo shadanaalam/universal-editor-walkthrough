611b357c436789ca1b357c163e288226d02b947f
shadanaalam <shadan.aalam@tothenew.com>
2025-09-11 13:27:44 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -22,4 +22,48 @@ export default function decorate(block) {
   });
   block.textContent = '';
   block.append(ul);
+
+   $.ajax({
+        url: "/content/ttnretail/us/en/sign-in.infinity.json",
+        type: "GET",
+        success: function (data) {
+            console.log("Data "+ data);
+        },
+        error: function () {
+            console.warn("Could not fetch page properties at: " + pageEndpoint);
+        }
+    });
+
+  try {
+    const res = await fetch("https://countriesnow.space/api/v0.1/countries/flag/images");
+    const json = await res.json();
+    console.log("Countries "+ json);
+
+    select.innerHTML = ''; // Clear loading text
+
+    const placeholderOption = document.createElement("option");
+    placeholderOption.value = "";
+    placeholderOption.textContent = "Select a country";
+    select.appendChild(placeholderOption);
+
+    json.data.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country.name;
+      option.textContent = country.name;
+      option.setAttribute("data-flag", country.flag); // Store flag URL
+      select.appendChild(option);
+    });
+
+    
+
+  } catch (err) {
+    console.error("Failed to fetch countries:", err);
+    select.innerHTML = "";
+    const errorOption = document.createElement("option");
+    errorOption.value = "";
+    errorOption.textContent = "Failed to load countries";
+    select.appendChild(errorOption);
+  }
+
+  
 }
